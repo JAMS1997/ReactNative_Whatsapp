0c3f1332304c6d54b5ff53a312e3b654f093f632
Jhon Melendez <jhonmelendez77@gmail.com>
2023-11-07 21:40:34 -0500

    InputBox
    Integracion del inputBox

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,20 +1,27 @@
 // src/screens/ChatScreen.js
 
-import { ImageBackground, StyleSheet, FlatList } from "react-native";
+import { ImageBackground, StyleSheet, FlatList, KeyboardAvoidingView } from "react-native";
 import bg from "../../assets/images/BG2.png";
 import Message from "../componentes/Message/index.js";
 import messages from "../../assets/data/messages.json";
+import InputBox from "../componentes/InputBox/index.js";
 
 const ChatScreen = () => {
   return (
-    <ImageBackground source={bg} style={styles.bg}>
-      <FlatList
-        data={messages}
-        renderItem={({ item }) => <Message message={item} />}
-				style={{ padding: 10 }}
-				inverted
-      />
-    </ImageBackground>
+    <KeyboardAvoidingView
+    behavior={Platform.OS === "ios" ? "padding" : "height"}
+    style={styles.bg}
+    >
+        <ImageBackground source={bg} style={styles.bg}>
+          <FlatList
+            data={messages}
+            renderItem={({ item }) => <Message message={item} />}
+            style={{ padding: 10 }}
+            inverted
+          />
+          <InputBox/>
+        </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 
